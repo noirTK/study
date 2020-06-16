d2e3734e397c26238070a754dde004c0990f68e9
noir8TK <plaire@hanyang.ac.kr>
2020-06-17 03:02:55 +0900

    update listQueue

---
+++ c/자료구조/queue/listQueue/listQueue.h
@@ -0,0 +1,24 @@
+#ifndef _LISTQUEUE_H_
+#define _LISTQUEUE_H_
+
+#include <stdio.h>
+#define MAX_INDEX 10000
+
+typedef int element;
+typedef struct
+{
+    int front, rear;
+    element data[MAX_INDEX];
+} listQueue;
+
+void init(listQueue* q);
+
+int empty(const listQueue* q);
+int full(const listQueue* q);
+int size(const listQueue* q);
+
+void push(listQueue* q, element item);
+element pop(listQueue* q);
+element front(const listQueue* q);
+element back(const listQueue* q);
+#endif
