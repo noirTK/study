f4c47c674f0e811b49d9bbc349b4ff2122b8229c
noir8TK <plaire@hanyang.ac.kr>
2020-06-18 23:09:30 +0900

    update 1929

---
+++ cpp/알고리즘/1929.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+void checkPrime(array<bool,1000001>* const nums)
+{
+    nums->fill(true);
+    nums->at(0) = nums->at(1) = false;
+    for(int i = 2; i < nums->size(); ++i)
+    {
+        if(nums->at(i) == true)
+        {
+            for(int j = i*2; j < nums->size() ; j+=i) nums->at(j) = false;
+        }
+    }  
+};
+
+int main()
+{
+    array<bool,1000001>* nums = new array<bool, 1000001>;
+    checkPrime(nums);
+    int min, max;
+    cin >> min >> max;
+    for(int it = min; it <= max; ++it)
+    {
+        if(nums->at(it) == true) cout << it << '\n';
+    }
+    return 0;
+}
