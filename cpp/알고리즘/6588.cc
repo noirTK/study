63d72c487c19b95e8bb44da83b631f50f8a26910
noir8TK <plaire@hanyang.ac.kr>
2020-06-19 00:56:17 +0900

    modified 6588 -20.06.19

--- cpp/알고리즘/6588.cc
+++ cpp/알고리즘/6588.cc
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <array>
 #include <string>
-
+#include <vector>
 using namespace std;
 
-void checkPrime(array<bool,1000001>* const nums)
+void checkPrime(array<bool,1000001>* const nums, vector<int>* prime)
 {
     nums->fill(true);
     nums->at(0) = nums->at(1) = false;
-    for(int i = 2; i < nums->size(); ++i) if(nums->at(i) == true) for(int j = i*2; j < nums->size(); j+=i) nums->at(j) = false;
-    nums->at(2) = false;
+    for(int i = 2; i < nums->size(); ++i)
+    {
+        if(nums->at(i) == true) 
+        {
+            prime->push_back(i);
+            for(int j = i*2; j < nums->size(); j+=i) 
+            {
+                nums->at(j) = false;
+            }
+        }
+    }
 }
 
 int main()
@@ -18,22 +27,24 @@ int main()
     cout.tie(nullptr);
     string input;
     array<bool,1000001>* nums = new array<bool,1000001>;
-    checkPrime(nums);
+    vector<int>* prime = new vector<int>;
+    checkPrime(nums,prime);
     while(getline(cin,input) && input != "0")
     {
         bool found = false;
         int num = stoi(input);
-        for(int i = 3; i <= num-3; i+=2)
+        for(auto it = prime->begin(); *it < num && it != prime->end(); ++it)
         {
-            if(nums->at(i) && nums->at(num-i) && !found)
+            if(nums->at(num-(*it)) && !found)
             {
                 found = true;
-                cout << num << " = " << i << " + " << num-i << '\n';
+                cout << num << " = " << *it << " + " << num-(*it) << '\n';
                 break;
             }
         }
         if(!found) cout << "Goldbach's conjecture is wrong.\n";
     }
     delete nums;
+    delete prime;
     return 0;
 }
