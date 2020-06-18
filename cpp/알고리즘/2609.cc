16a5da6253a5cf3c7a10299f8015edb6d91c15f8
noir8TK <plaire@hanyang.ac.kr>
2020-06-18 20:42:33 +0900

    update 2609

---
+++ cpp/알고리즘/2609.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <array>
+
+int GCD(int a, int b)
+{
+    while(b!=0)
+    {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int LCM(int a, int b)
+{
+    return a*b/GCD(a,b);
+}
+
+int main()
+{
+    std::array<int,3> nums;
+    std::cin >> nums[0] >> nums[1];
+    std::cout << GCD(nums[0],nums[1]) << '\n';
+    std::cout << LCM(nums[0],nums[1]) << '\n';
+    return 0;
+}
