2c95f4f469802fc133fe2d75adda3b5d89c2bc0b
noir8TK <plaire@hanyang.ac.kr>
2020-06-27 17:02:13 +0900

    modified 2133 TD

--- cpp/알고리즘/2133_td.cc
+++ cpp/알고리즘/2133_td.cc
@@ -8,8 +8,7 @@ int sol(const int n)
     if(n == 0) return 1;
     if(n % 2 == 1) return 0;
     if(d[n] != -1) return d[n];
-    d[n] = 0;
-    d[n] += sol(n-2) * 3;
+    d[n] = sol(n-2) * 3;
     for(int i = 4; i <= n; i+=2) d[n] += sol(n-i) * 2;
     return d[n];
 }
