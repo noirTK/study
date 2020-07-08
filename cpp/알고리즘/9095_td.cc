f7be42cc3ecf5de9d6a49a85b3c7bfcfa89f8cb6
noir8TK <plaire@hanyang.ac.kr>
2020-07-08 13:41:28 +0900

    update 9095 TD

--- cpp/알고리즘/9095_td.cc
+++ cpp/알고리즘/9095_td.cc
@@ -1,25 +1,31 @@
+/*
+https://www.acmicpc.net/problem/9095
+9095 다이나믹 문제풀이 Top Down
+sol(n) 정수 n에 대하여 1,2,3의 합으로 나타낼 수 있는 경우의 수
+sol(n) = sol(n-3) + sol(n-2) + sol(n-1)
+sol(0) = sol(1) = 1
+sol(2) = 2
+*/
 #include <iostream>
 #include <array>
 
 using namespace std;
 array<int, 11> d;
-
 int sol(const int n)
 {
-    if(n == 0 or n == 1) return 1;
-    else if(n == 2) return 2;
-    if(d[n] > 0) return d[n];
-    d[n] = sol(n-1) + sol(n-2) + sol(n-3);
+    if(n == 0 || n == 1) return 1;
+    if(n == 2) return 2;
+    if(d[n] != -1) return d[n];
+    d[n] = sol(n-3) + sol(n-2) + sol(n-1);
     return d[n];
 }
-
 int main()
 {
-    int t;
+    int t, n;
     cin >> t;
     while(t--)
     {
-        int n;
+        d.fill(-1);
         cin >> n;
         cout << sol(n) << '\n';
     }
