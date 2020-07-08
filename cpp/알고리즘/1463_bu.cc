7eab6a3864de8b7885ee34fb1809547e16e29eda
noir8TK <plaire@hanyang.ac.kr>
2020-07-08 13:01:20 +0900

    update 1463 BU

--- cpp/알고리즘/1463_bu.cc
+++ cpp/알고리즘/1463_bu.cc
@@ -1,25 +1,26 @@
+/*
+https://www.acmicpc.net/problem/1463
+1463 다이나믹 문제풀이 - Bottom Up
+d[i] = 정수 i에 대해 1로 만드는 연산의 최솟값
+d[i] = min(d[i-1]+1, d[i/3]+1, d[i/2]+1)
+d[1] = 0
+*/
+
 #include <iostream>
 #include <array>
-
 using namespace std;
-array<int, 1000001> arr = {0};
+array<int, 1000001> d;
 int main()
 {
     int n;
     cin >> n;
-    arr[1] = 0;
+    d[1] = 0;
     for(int i = 2; i <= n; ++i)
     {
-        arr[i] = arr[i-1] + 1;
-        if(i % 2 == 0 && arr[i] > arr[i/2] + 1)
-        {
-            arr[i] = arr[i/2] + 1;
-        }
-        if(i % 3 == 0 && arr[i] > arr[i/3] + 1)
-        {
-            arr[i] = arr[i/3] + 1;
-        }
+        d[i] = d[i-1] + 1;
+        if(i % 3 == 0 && d[i] > d[i/3] + 1) d[i] = d[i/3] + 1;
+        if(i % 2 == 0 && d[i] > d[i/2] + 1) d[i] = d[i/2] + 1;
     }
-    cout << arr[n] << '\n'; 
+    cout << d[n] << '\n';
     return 0;
 }
