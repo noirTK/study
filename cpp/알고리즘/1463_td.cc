0fc3c140a5559a58ad837ac9e96344305c765450
noir8TK <plaire@hanyang.ac.kr>
2020-07-08 12:53:40 +0900

    update 1463 TD

--- cpp/알고리즘/1463_td.cc
+++ cpp/알고리즘/1463_td.cc
@@ -1,30 +1,30 @@
+/*
+https://www.acmicpc.net/problem/1463
+1463 다이나믹 문제풀이 - TopDown
+D[n] = 정수 n에 대해 1로 만드는 연산 횟수의 최솟값
+D[n] = min(d[n/3]+1, d[n/2]+1, d[n-1]+1)
+d[1] = 0
+*/
+
 #include <iostream>
 #include <array>
-
 using namespace std;
-array<int,1000001> d = {0};
-int goto1(const int n)
+array<int, 1000001> d;
+int sol(const int n)
 {
     if(n == 1) return 0;
-    if(d[n] > 0) return d[n];
-    d[n] = goto1(n-1) + 1;
-    if(n % 2 == 0)
-    {
-        int temp = goto1(n/2) + 1;
-        if(d[n] > temp) d[n] = temp;
-    }
-    if(n % 3 == 0)
-    {
-        int temp = goto1(n/3) + 1;
-        if(d[n] > temp) d[n] = temp;
-    }
+    if(d[n] != -1) return d[n];
+    d[n] = sol(n-1) + 1;
+    if(n % 3 == 0 && d[n] > sol(n/3) + 1) d[n] = sol(n/3) + 1;
+    if(n % 2 == 0 && d[n] > sol(n/2) + 1) d[n] = sol(n/2) + 1;
     return d[n];
 }
 
 int main()
 {
+    d.fill(-1);
     int n;
     cin >> n;
-    cout << goto1(n) << '\n';
+    cout << sol(n) << '\n';
     return 0;
 }
