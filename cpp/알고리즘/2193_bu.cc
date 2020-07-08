9dfc4b27010728a64bcfed2579b2c20b1ca6b432
noir8TK <plaire@hanyang.ac.kr>
2020-07-09 00:25:25 +0900

    update 2193 TD && BU

--- cpp/알고리즘/2193_bu.cc
+++ cpp/알고리즘/2193_bu.cc
@@ -1,22 +1,27 @@
+/*
+https://www.acmicpc.net/problem/2193
+2193 다이나믹 문제풀이 Bottom Up
+d[n][t] n자리수의 끝수가 t인 경우의 이친수의 개수
+d[n][1] = d[n-1][0]
+d[n][0] = d[n-1][0] + d[n-1][1]
+d[1][0] = 0
+d[1][1] = 1
+*/
+
 #include <iostream>
 #include <array>
 
 using namespace std;
 array<array<long long, 2>, 91> d;
-
 int main()
 {
     int n;
     cin >> n;
-    d[1][0] = 0;
     d[1][1] = 1;
-    for(int i = 2; i < 91; ++i)
+    for(int i = 2; i <= n; ++i)
     {
-        for(int j = 0; j < 2; ++j)
-        {
-            if(j == 1) d[i][j] = d[i-1][0];
-            else d[i][j] = d[i-1][0] + d[i-1][1];
-        }
+        d[i][1] = d[i-1][0];
+        d[i][0] = d[i-1][0] + d[i-1][1];
     }
     cout << d[n][0] + d[n][1] << '\n';
     return 0;
--- cpp/알고리즘/2193_td.cc
+++ cpp/알고리즘/2193_td.cc
@@ -1,24 +1,32 @@
+/*
+https://www.acmicpc.net/problem/2193
+2193 다이나믹 문제풀이 Top Down
+sol(n,t) n자리 수의 끝 수가 t인 경우의 이친수의 개수
+sol(n,1) = sol(n-1,0)
+sol(n,0) = sol(n-1,0) + sol(n-1,1)
+sol(1,1) = 1
+sol(1,0) = 0
+*/
+
 #include <iostream>
 #include <array>
 
 using namespace std;
-array<array<long long ,2>, 91> d;
+array<array<long long, 2>, 91> d;
 
 long long sol(const int n, const int t)
 {
-    if(n == 1) return (t == 1 ? 1 : 0);
+    if(n == 1) return t == 1 ? 1 : 0;
     if(d[n][t] != -1) return d[n][t];
-    if(t == 0) d[n][t] = sol(n-1, 0) + sol(n-1, 1);
-    else d[n][t] = sol(n-1, 0);
-    
+    d[n][t] = (t == 1) ? sol(n-1, 0) : sol(n-1,0) + sol(n-1,1);
     return d[n][t];
 }
 
 int main()
 {
-    for(int i = 1; i < 91; ++i) d[i].fill(-1);
     int n;
     cin >> n;
+    for(int i = 1; i <= n; ++i) d[i].fill(-1);
     cout << sol(n,0) + sol(n,1) << '\n';
     return 0;
 }
