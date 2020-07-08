c084124aab1b834a604ae4929219ca1bc2ee3408
noir8TK <plaire@hanyang.ac.kr>
2020-07-09 00:05:06 +0900

    update 10844 TD && BU

--- cpp/알고리즘/10844_bu.cc
+++ cpp/알고리즘/10844_bu.cc
@@ -1,31 +1,44 @@
+/*
+https://www.acmicpc.net/problem/10844
+10844 다이나믹 문제풀이 Bottom Up
+d[n][t] 길이가 n이고 끝의 수가 t인 경우의 계단 수의 개수
+(t == 0) d[n][t] = d[n-1][1]
+(t == 9) d[n][t] = d[n-1][8]
+(0 < t < 9) d[n][t] = d[n-1][t-1] + d[n-1][t+1]
+d[1][0] = 0 d[1][L] = 1 (0 < L <= 9)
+*/
+
 #include <iostream>
 #include <array>
 
 using namespace std;
-array<array<long long, 10>, 101> d;
 const long long mod = 1000000000LL;
+array<array<long long, 10>, 101> d;
 
 int main()
 {
     int n;
     cin >> n;
-    for(int i = 0; i < 10; ++i) 
-    {
-        d[0][i] = 0;
-        d[1][i] = (i == 0 ? 0 : 1);
-    }
+    long long ans = 0;
+    for(int L = 1; L <= 9; ++L) d[1][L] = 1;
     for(int i = 2; i <= n; ++i)
     {
-        for(int j = 0; j < 10; ++j)
+        for(int t = 0; t <= 9; ++t)
         {
-            d[i][j] = 0;
-            if(j - 1 >= 0) d[i][j] += d[i-1][j-1];
-            if(j + 1 <= 9) d[i][j] += d[i-1][j+1];
-            d[i][j] %= mod;
+            if(t == 0) d[i][t] = d[i-1][1];
+            else if(t == 9) d[i][t] = d[i-1][8];
+            else 
+            {
+                d[i][t] = d[i-1][t-1] + d[i-1][t+1];
+                d[i][t] %= mod;
+            }
         }
     }
-    long long ans = 0;
-    for(int i = 0; i < 10; ++i) ans += d[n][i];
-    cout << ans % mod << '\n';
+    for(int i = 0; i <= 9; ++i)
+    {
+        ans += d[n][i];
+        ans %= mod;
+    }
+    cout << ans << '\n';
     return 0;
 }
--- cpp/알고리즘/10844_td.cc
+++ cpp/알고리즘/10844_td.cc
@@ -1,3 +1,13 @@
+/*
+https://www.acmicpc.net/problem/10844
+10844 다이나믹 문제풀이 Top Down
+sol(n,t) 길이가 n이고 끝의 수가 t인 경우의 계단 수의 개수
+(t == 0) sol(n,t) = sol(n-1,1)
+(t == 9) sol(n,t) = sol(n-1,8)
+(0 < t < 9) sol(n,t) = sol(n-1,t-1) + sol(n-1,t+1)
+sol(1,0) = 0 sol(1, L) = 1 (0 < L <= 9)
+*/
+
 #include <iostream>
 #include <array>
 
@@ -7,30 +17,29 @@ array<array<long long, 10>, 101> d;
 
 long long sol(const int n, const int t)
 {
-    if(n == 0) return 0;
-    if(n == 1) return (t == 0 ? 0 : 1);
+    if(n == 1) return t == 0 ? 0 : 1;
     if(d[n][t] != -1) return d[n][t];
-
-    if(t == 0) d[n][t] = sol(n-1, t+1);
-    else if(t == 9) d[n][t] = sol(n-1, t-1);
+    if(t == 0) d[n][t] = sol(n-1,1);
+    else if(t == 9) d[n][t] = sol(n-1,8);
     else 
     {
-        d[n][t] = sol(n-1, t+1) + sol(n-1, t-1);
+        d[n][t] = sol(n-1,t-1) + sol(n-1, t+1);
         d[n][t] %= mod;
     }
     return d[n][t];
 }
+
 int main()
 {
-    long long ans = 0;
-    for(int i = 1; i < 101; ++i) d[i].fill(-1);
     int n;
     cin >> n;
-    for(int i = 0; i < 10; ++i)
+    for(int i = 1; i <= n; ++i) d[i].fill(-1);
+    long long ans = 0;
+    for(int i = 0; i <= 9; ++i)
     {
-        ans += sol(n, i);
+        ans += sol(n,i);
         ans %= mod;
     }
-    cout << ans % mod << '\n';
+    cout << ans << '\n';
     return 0;
 }
