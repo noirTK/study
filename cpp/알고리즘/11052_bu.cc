e5877eef54854f74de5f7bb761c5d6e6822f4d05
noir8TK <plaire@hanyang.ac.kr>
2020-07-08 16:45:58 +0900

    update 11052 TD && BU

--- cpp/알고리즘/11052_bu.cc
+++ cpp/알고리즘/11052_bu.cc
@@ -1,30 +1,30 @@
+/*
+https://www.acmicpc.net/problem/11052
+11052 다이나믹 문제풀이 - Bottom Up
+d[n] = 카드 n개를 구입하기 위해 지불해야하는 금액의 최댓값
+d[n] = max(d[n-j] + p[j]) (1 <= j <= n)
+d[0] = 0
+*/
+
 #include <iostream>
 #include <array>
+#include <algorithm>
 
 using namespace std;
-array<int, 1001> d = {0};
-array<int, 1001> p;
-
+array<int, 1001> d, p;
 int main()
 {
     int n;
     cin >> n;
-    for(int i = 1; i <= n; ++i)
-    {
-        int value;
-        cin >> value;
-        p[i] = value;
-    }
-
+    for(int i = 1; i <= n; ++i) cin >> p[i];
     d[0] = 0;
     for(int i = 1; i <= n; ++i)
     {
         for(int j = 1; j <= i; ++j)
         {
-            if(d[i] < d[i-j] + p[j]) d[i] = d[i-j] + p[j];
+            d[i] = max(d[i], d[i-j] + p[j]);
         }
     }
-
     cout << d[n] << '\n';
     return 0;
 }
--- cpp/알고리즘/11052_td.cc
+++ cpp/알고리즘/11052_td.cc
@@ -1,31 +1,32 @@
+/*
+https://www.acmicpc.net/problem/11052
+11052 다이나믹 문제풀이 - Top Down
+sol(n) = 정수 n에 대하여 n개의 카드를 구입할 경우 지불해야하는 금액의 최댓값
+sol(n) = max(sol(n-j) + p[j]) (1 <= j <= n)
+sol(0) = 0
+*/
+
 #include <iostream>
 #include <array>
+#include <algorithm>
 
 using namespace std;
-array<int, 1001> d, p = {0};
+array<int, 1001> p, d;
 
 int sol(const int n)
 {
-    if(n == 0 ) return 0;
-    if(d[n] > 0) return d[n];
-    for(int j = 1; j <= n; ++j)
-    {
-        d[n] = max(d[n],sol(n-j) + p[j]);
-    }
+    if(n == 0) return 0;
+    if(d[n] != -1) return d[n];
+    for(int j = 1; j <= n; ++j) d[n] = max(d[n], sol(n-j) + p[j]);
     return d[n];
 }
 
 int main()
 {
+    d.fill(-1);
     int n;
     cin >> n;
-    for(int i = 1; i <= n; ++i)
-    {
-        int v;
-        cin >> v;
-        p[i] = v;
-    }
-
+    for(int i = 1; i <= n; ++i) cin >> p[i];
     cout << sol(n) << '\n';
     return 0;
 }
