87c955d44326849f95334ee05d8ae73f819fe36d
noir8TK <plaire@hanyang.ac.kr>
2020-07-09 23:53:48 +0900

    update 15988 TD && BU

--- cpp/알고리즘/15988_bu.cc
+++ cpp/알고리즘/15988_bu.cc
@@ -1,26 +1,43 @@
+/*
+https://www.acmicpc.net/problem/15988
+15988 다이나믹 문제풀이 Bottom Up
+d[n] 정수 n에 대하여 1,2,3의 합으로 나타내는 방법 수
+d[n] = Σ d[n-L] (1 <= L <= 3)
+d[0] = d[1] = 1
+d[2] = 2
+=====================================================
+t가 주워져서 t번 값을 print 해야하는데
+각 t 마다 값이 항상 일정하므로 
+t의 while문 밖에서 값을 미리 구해 놓고 바로 인덱스 접근하도록 했다.
+while문 안에서 값을 구하면 시간초과 됨.
+=====================================================
+*/
 #include <iostream>
 #include <array>
 
 using namespace std;
-array<long long, 1000001> d;
 const long long mod = 1000000009LL;
+array<long long, 1000001> d;
 
 int main()
 {
+    d[0] = d[1] = 1;
+    d[2] = 2;
     int t;
     cin >> t;
+    for(int i = 3; i <= 1000000; ++i)
+    {
+        d[i] = 0;
+        for(int L = 1; L <= 3; ++L)
+        {
+            d[i] += d[i-L];
+            d[i] %= mod;
+        }
+    } 
     while(t--)
     {
         int n;
         cin >> n;
-        d[0] = d[1] = 1;
-        d[2] = 2;
-        for(int i = 3; i <= n; ++i)
-        {
-            d[i] = 0;
-            for(int j = 1; j <= 3; ++j) d[i] += d[i-j];
-            d[i] %= mod;
-        }
         cout << d[n] << '\n';
     }
     return 0;
--- cpp/알고리즘/15988_td.cc
+++ cpp/알고리즘/15988_td.cc
@@ -1,25 +1,37 @@
+/*
+https://www.acmicpc.net/problem/15988
+15988 다이나믹 문제풀이 Top Down
+sol(n) 정수 n에 대하여 1, 2, 3의 합으로 나타내는 방법의 수
+sol(n) = Σ sol(n-L) (1 <= L <= 3)
+sol(0) = sol(1) = 1
+sol(2) = 2
+*/
 #include <iostream>
 #include <array>
 
 using namespace std;
-array<long long, 1000001> d;
 const long long mod = 1000000009LL;
+array<long long, 1000001> d;
+
 long long sol(const int n)
 {
     if(n == 0 || n == 1) return 1;
     if(n == 2) return 2;
     if(d[n] != -1) return d[n];
     d[n] = 0;
-    for(int i = 1; i <= 3; ++i) d[n] += sol(n-i);
-    d[n] %= mod;
+    for(int L = 1; L <= 3; ++L)
+    {
+        d[n] += sol(n-L);
+        d[n] %= mod;
+    }
     return d[n];
 }
 
 int main()
 {
-    d.fill(-1);
     int t;
     cin >> t;
+    d.fill(-1);
     while(t--)
     {
         int n;
