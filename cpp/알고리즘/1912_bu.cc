925ffdea92edaf50d0980366460d3a7d6e4491f8
noir8TK <plaire@hanyang.ac.kr>
2020-07-09 16:57:58 +0900

    update 1912 TD && BU

--- cpp/알고리즘/1912_bu.cc
+++ cpp/알고리즘/1912_bu.cc
@@ -1,5 +1,14 @@
+/*
+https://www.acmicpc.net/problem/1912
+1912 다이나믹 문제풀이 Bottom Up
+d[n] 길이가 n인 수열의 마지막이 n인 경우의 연속합
+d[n] = max(d[n-1]+p[n], p[n])
+d[1] = p[1]
+*/
+
 #include <iostream>
 #include <array>
+#include <algorithm>
 
 using namespace std;
 array<int, 100001> p, d;
@@ -8,20 +17,12 @@ int main()
 {
     int n;
     cin >> n;
-    for(int i = 1; i <= n; ++i)
-    {
-        int t;
-        cin >> t;
-        p[i] = t;
-    }
+    for(int i = 1; i <= n; ++i) cin >> p[i];
     d[1] = p[1];
     for(int i = 2; i <= n; ++i)
     {
-        if(d[i-1] + p[i] > p[i]) d[i] = d[i-1] + p[i];
-        else d[i] = p[i];
+        d[i] = max(d[i-1] + p[i], p[i]);
     }
-    int ans;
-    for(int i = 1; i <= n; ++i) if(i == 1 || ans < d[i]) ans = d[i];
-    cout << ans << '\n';
+    cout << *max_element(d.cbegin()+1, d.cbegin()+n+1) << '\n';
     return 0;
 }
--- cpp/알고리즘/1912_td.cc
+++ cpp/알고리즘/1912_td.cc
@@ -1,34 +1,37 @@
+/*
+https://www.acmicpc.net/problem/1912
+1912 다이나믹 문제풀이 Top Down
+sol(n) 마지막이 n인 연속합
+sol(n) = max(sol(n-1)+p[n], p[n])
+sol(1) = p[1]
+*/
+
 #include <iostream>
 #include <array>
+#include <algorithm>
 
 using namespace std;
-array<int, 100001> p, d;
+array<int, 100001> d, p;
 
 int sol(const int n)
 {
     if(n == 1)
     {
         d[1] = p[1];
-        return d[1];
+        return p[1];
     }
-    if(d[n] != -1) return d[n];
-    d[n] = (sol(n-1)+p[n] > p[n] ? sol(n-1)+p[n] : p[n]);
+    if(d[n] != -1001) return d[n];
+    d[n] = max(sol(n-1) + p[n], p[n]);
     return d[n];
 }
+
 int main()
 {
-    d.fill(-1);
+    d.fill(-1001);
     int n;
     cin >> n;
-    for(int i = 1; i <= n; ++i)
-    {
-        int t;
-        cin >> t;
-        p[i] = t;
-    }
+    for(int i = 1; i <= n; ++i) cin >> p[i];
     sol(n);
-    int ans;
-    for(int i = 1; i <= n; ++i) if(i == 1 || ans < d[i]) ans = d[i];
-    cout << ans << '\n';
+    cout << *max_element(d.cbegin()+1, d.cbegin()+n+1) << '\n';
     return 0;
 }
