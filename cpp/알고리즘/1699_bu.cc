df96b05ee53b90564f6405004f5b0b85f45861d6
noir8TK <plaire@hanyang.ac.kr>
2020-07-09 17:54:22 +0900

    update 1699 TD && BU

--- cpp/알고리즘/1699_bu.cc
+++ cpp/알고리즘/1699_bu.cc
@@ -1,1 +1,30 @@
+/*
+https://www.acmicpc.net/problem/1699
+1699 다이나믹 문제풀이 Bottom Up
+d[n] 정수 n에 대해 제곱수의 합의 항의 최소 갯수
+d[n] = min(d[n-L*L]+1,d[n]) (1<= L*L <= n)
+d[0] = 0
+*/
+
 #include <iostream>
+#include <array>
+#include <algorithm>
+using namespace std;
+array<int, 100001> d;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; ++i)
+    {
+        d[i] = i;
+        for(int k = 1; k*k <= i; ++k)
+        {
+            d[i] = min(d[i-k*k]+1, d[i]);
+        }
+    }
+    cout << d[n] << '\n';
+    return 0;
+}
--- cpp/알고리즘/1699_td.cc
+++ cpp/알고리즘/1699_td.cc
@@ -1,17 +1,25 @@
+/*
+https://www.acmicpc.net/problem/1699
+1699 다이나믹 문제풀이 Top Down
+sol(n) 정수 n의 제곱수의 항의 최소갯수
+sol(n) = min(sol(n-L*L), d[n]) (1 <= L*L <= n)
+sol(0) = 0
+*/
+
 #include <iostream>
 #include <array>
-
+#include <algorithm>
 using namespace std;
 array<int, 100001> d;
 
 int sol(const int n)
 {
-    if(n == 1) return 1;
+    if(n == 0) return 0;
     if(d[n] != -1) return d[n];
     d[n] = n;
-    for(int k = 1; k*k <= n; ++k)
+    for(int i = 1; i*i <= n; ++i)
     {
-        d[n] = (d[n] > sol(n-k*k) ? sol(n-k*k)+1 : d[n]); 
+        d[n] = min(sol(n-i*i) + 1, d[n]);
     }
     return d[n];
 }
