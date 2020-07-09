bf1bf2019795db3119205779165f396a3652325f
noir8TK <plaire@hanyang.ac.kr>
2020-07-10 01:02:16 +0900

    update 1149 TD && BU

--- cpp/알고리즘/1149_bu.cc
+++ cpp/알고리즘/1149_bu.cc
@@ -1,21 +1,33 @@
+/*
+https://www.acmicpc.net/problem/1149
+1149 다이나믹 문제풀이 Bottom Up
+d[n][k] n개의 집 중 마지막 집을 k색으로 색칠시 최소 비용
+d[n][R] = min(d[n-1][G], d[n-1][B]) + p[n][R]
+d[n][G] = min(d[n-1][R], d[n-1][B]) + p[n][G]
+d[n][B] = min(d[n-1][R], d[n-1][G]) + p[n][B]
+d[0][c] = 0 (c = R || c = G || c = B)이므로, n = 1인 경우에도 위의 점화식이 성립한다.
+ans = min(d[n][R], d[n][G], d[n][B])
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
+
 using namespace std;
 array<array<int, 3>, 1001> d, p;
+enum colar { R = 0, G, B};
 
 int main()
 {
     int n;
     cin >> n;
-    for(int i = 1; i <= n; ++i) cin >> p[i][0] >> p[i][1] >> p[i][2];
-    for(int i = 0; i < 3; ++i) d[1][i] = p[1][i];
-    for(int i = 2; i <= n; ++i)
+    for(int i = 1; i <= n; ++i) cin >> p[i][R] >> p[i][G] >> p[i][B];
+    for(int i = 1; i <= n; ++i)
     {
-        d[i][0] = min({d[i-1][1], d[i-1][2]}) + p[i][0];
-        d[i][1] = min({d[i-1][0], d[i-1][2]}) + p[i][1];
-        d[i][2] = min({d[i-1][1], d[i-1][0]}) + p[i][2];
+        d[i][R] = min(d[i-1][G], d[i-1][B]) + p[i][R];
+        d[i][G] = min(d[i-1][R], d[i-1][B]) + p[i][G];
+        d[i][B] = min(d[i-1][R], d[i-1][G]) + p[i][B];
     }
-    cout << min({d[n][0], d[n][1], d[n][2]}) << '\n';
+    cout << *min_element(&d[n][0], &d[n][0]+3) << '\n';
     return 0;
 }
--- cpp/알고리즘/1149_td.cc
+++ cpp/알고리즘/1149_td.cc
@@ -1,25 +1,38 @@
+/*
+https://www.acmicpc.net/problem/1149
+1149 다이나믹 문제풀이 Top Down
+sol(n,c) n개의 집 중 마지막 집을 c색으로 색칠 시 최소비용
+sol(n,R) = min(sol(n-1,B), sol(n-1,G)) + p[n][R]
+sol(n,G) = min(sol(n-1,R), sol(n-1,B)) + p[n][G]
+sol(n,B) = min(sol(n-1,R), sol(n-1,G)) + p[n][B]
+sol(1,c) = p[1][c] (c = R || c = B || c = C)
+ans = min(sol(n,R), sol(n,G), sol(n,B))
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
+
 using namespace std;
+enum color { R = 0, G, B };
 array<array<int, 3>, 1001> d, p;
 
-int sol(const int n, const int t)
+int sol(const int n, const int c)
 {
-    if(n == 1) return p[1][t];
-    if(d[n][t] != -1) return d[n][t];
-    if(t == 0) d[n][t] = min({sol(n-1, 1), sol(n-1, 2)}) + p[n][t];
-    if(t == 1) d[n][t] = min({sol(n-1, 0), sol(n-1, 2)}) + p[n][t];
-    if(t == 2) d[n][t] = min({sol(n-1, 1), sol(n-1, 0)}) + p[n][t];
-    return d[n][t];
+    if(n == 1) return p[1][c];
+    if(d[n][c] != -1) return d[n][c];
+    if(c == R) d[n][c] = min(sol(n-1,B),sol(n-1,G)) + p[n][R];
+    else if(c == G) d[n][c] = min(sol(n-1,R),sol(n-1,B)) + p[n][G];
+    else if(c == B) d[n][c] = min(sol(n-1,R),sol(n-1,G)) + p[n][B];
+    return d[n][c];
 }
 
 int main()
 {
-    for(int i = 1; i < 1001; ++i) d[i].fill(-1);
     int n;
     cin >> n;
-    for(int i = 1; i <= n; ++i) cin >> p[i][0] >> p[i][1] >> p[i][2];
-    cout << min({sol(n, 0), sol(n, 1), sol(n, 2)}) << '\n';
+    for(int i = 1; i <= n; ++i) cin >> p[i][R] >> p[i][G] >> p[i][B];
+    for(int i = 1; i <= n; ++i) d[i].fill(-1);
+    cout << min({sol(n,R), sol(n,G), sol(n,B)}) << '\n';
     return 0;
 }
