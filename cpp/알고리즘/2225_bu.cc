190f3ddc40c5465a6585a67935a0e45e4516b3d3
noir8TK <plaire@hanyang.ac.kr>
2020-07-09 23:19:28 +0900

    update 2225 TD && BU

--- cpp/알고리즘/2225_bu.cc
+++ cpp/알고리즘/2225_bu.cc
@@ -1,23 +1,37 @@
+/*
+https://www.acmicpc.net/problem/2225
+2225 다이나믹 문제풀이 Bottom Up
+d[n][k] 정수 n에 대하여 0에서 n미만의 수 k개 더해서 합이 n이 되는 경우의 수
+d[n][k] = Σ d[n-L][k-1] (0 <= L <= n, k != -1)
+d[S][1] = 1 (0 <= S <= n)
+*/
+
 #include <iostream>
 #include <array>
 
-const long long mod = 1000000000LL;
 using namespace std;
+const long long mod = 1000000000LL;
 array<array<long long, 201>, 201> d;
+
 int main()
 {
     int n, k;
     cin >> n >> k;
-    d[0][0] = 1LL;
+    
     for(int i = 0; i <= n; ++i)
     {
-        for(int l = 1; l <= k; ++l)
+        for(int j = 1; j <= k; ++j)
         {
-            for(int j = 0; j <= i; ++j)
+            if(j == 1) 
             {
-                d[i][l] += d[i-j][l-1];
+                d[i][j] = 1;
+                continue;
             }
-            d[i][l] %= mod;
+            d[i][j] = 0;
+            for(int L = 0; L <= i; ++L)
+            {
+                d[i][j] += d[i-L][j-1];
+                d[i][j] %= mod;
         }
     }
     cout << d[n][k] << '\n';
--- cpp/알고리즘/2225_td.cc
+++ cpp/알고리즘/2225_td.cc
@@ -1,24 +1,36 @@
+/*
+https://www.acmicpc.net/problem/2225
+2225 다이나믹 문제풀이 Top Down
+sol(n) 정수 n을 0 ~ n의 정수 k개를 더해서 나타낼수 있는 경우의 수
+sol(n,k) = Σ sol(n-L,k-1) (0 <= L <= n)
+sol(S,1) = 1 (0 <= S <= n)
+*/
+
 #include <iostream>
 #include <array>
 
 using namespace std;
-array<array<long long, 201>, 201> d;
 const long long mod = 1000000000LL;
+array<array<long long, 201>, 201> d;
 
-long long sol(const int n, const int k)
+long long sol(const int n, const int t)
 {
-    if(k == 1) return 1;
-    if(d[n][k] != -1) return d[n][k];
-    d[n][k] = 0;
-    for(int i = 0; i <= n; ++i) d[n][k] += sol(n-i,k-1);
-    d[n][k] %= mod;
-    return d[n][k];
+    if(t == 1) return 1;
+    if(d[n][t] != -1) return d[n][t];
+    d[n][t] = 0;
+    for(int L = 0; L <= n; ++L)
+    {
+        d[n][t] += sol(n-L, t-1);
+        d[n][t] %= mod;
+    }
+    return d[n][t];
 }
+
 int main()
 {
-    for(int i = 0; i < 201; ++i) d[i].fill(-1);
     int n, k;
     cin >> n >> k;
+    for(int i = 0; i <= n; ++i) d[i].fill(-1);
     cout << sol(n, k) << '\n';
     return 0;
 }
