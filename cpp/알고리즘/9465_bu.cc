baa3b72c18482f9cf2f5dea960634ad4b700f4be
noir8TK <plaire@hanyang.ac.kr>
2020-07-10 22:44:09 +0900

    update 9465 TD && BU

--- cpp/알고리즘/9465_bu.cc
+++ cpp/알고리즘/9465_bu.cc
@@ -1,9 +1,21 @@
+/*
+https://www.acmicpc.net/problem/9465
+9465 다이나믹 문제풀이 Bottom Up
+d[n][c] 2n 스티커 중에서 n번째를 선택하거나 선택하지 않았을 경우의 스티커 점수의 최댓값
+d[n][L] = max(d[n-1][N], d[n-1][R]) + p[n][L]
+d[n][R] = max(d[n-1][N], d[n-1][L]) + p[n][R]
+d[n][N] = max({d[n-1][N], d[n-1][R], d[n-1][L]})
+d[1][c] = p[1][c]
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
 
 using namespace std;
 array<array<int, 3>, 100001> d, p;
+enum choice {R = 0, L, N};
+
 int main()
 {
     int t;
@@ -12,20 +24,23 @@ int main()
     {
         int n;
         cin >> n;
-        for(int i = 0; i < 2; ++i)
+        for(int j = R; j <= L; ++j)
         {
-            for(int j = 1; j <= n; ++j) cin >> p[j][i];
+            for(int i = 1; i <= n; ++i)
+            {
+                cin >> p[i][j];
+            }
         }
-        d[1][0] = p[1][0];
-        d[1][1] = p[1][1];
-        d[1][2] = 0;
+        d[1][R] = p[1][R];
+        d[1][L] = p[1][L];
+
         for(int i = 2; i <= n; ++i)
         {
-            d[i][0] = max({d[i-1][1], d[i-1][2]}) + p[i][0];
-            d[i][1] = max({d[i-1][0], d[i-1][2]}) + p[i][1];
-            d[i][2] = max({d[i-1][0], d[i-1][1], d[i-1][2]});
+            d[i][L] = max(d[i-1][N], d[i-1][R]) + p[i][L];
+            d[i][R] = max(d[i-1][N], d[i-1][L]) + p[i][R];
+            d[i][N] = max({d[i-1][N], d[i-1][R], d[i-1][L]});
         }
-        cout << max({d[n][0], d[n][1], d[n][2]}) << '\n';
+        cout << max({d[n][L], d[n][R], d[n][N]}) << '\n';
     }
     return 0;
 }
--- cpp/알고리즘/9465_td.cc
+++ cpp/알고리즘/9465_td.cc
@@ -1,18 +1,30 @@
+/*
+https://www.acmicpc.net/problem/9465
+9465 다이나믹 문제풀이 Top Down
+sol(n,c) 2n 스티커 중에서 n번째 스티커를 선택하거나 선택하지 않았을 경우의 스티커 점수의 최댓값
+sol(n,L) = max(sol(n-1,N), sol(n-1,R)) + p[n][L]
+sol(n,R) = max(sol(n-1,N), sol(n-1,L)) + p[n][R]
+sol(n,N) = max(sol(n-1,N), sol(n-1,R), sol(n-1,L))
+sol(1,c) = p[1][c]
+** 각각의 케이스t 마다 d[n][c] 값이 변하므로 반드시 각 케이스 마다 -1로 초기화해야 한다.
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
 using namespace std;
 
 array<array<int, 3>, 100001> d, p;
+enum choice {R = 0, L, N};
 
-int sol(const int n, const int t)
+int sol(const int n, const int c)
 {
-    if(n == 1) return p[n][t];
-    if(d[n][t] != -1) return d[n][t];
-    if(t == 0) d[n][t] = max({sol(n-1,1), sol(n-1,2)}) + p[n][t];
-    if(t == 1) d[n][t] = max({sol(n-1,0), sol(n-1,2)}) + p[n][t];
-    if(t == 2) d[n][t] = max({sol(n-1,0),sol(n-1,1), sol(n-1,2)});
-    return d[n][t];
+    if(n == 1) return p[n][c];
+    if(d[n][c] != -1) return d[n][c];
+    if(c == L) d[n][c] = max(sol(n-1,N), sol(n-1,R)) + p[n][L];
+    else if(c == R) d[n][c] = max(sol(n-1,N), sol(n-1,L)) + p[n][R];
+    else if(c == N) d[n][c] = max({sol(n-1,N), sol(n-1,R), sol(n-1, L)});
+    return d[n][c];
 }
 
 int main()
@@ -21,14 +33,17 @@ int main()
     cin >> t;
     while(t--)
     {
-        for(int i = 1; i < 100001; ++i) d[i].fill(-1);
         int n;
         cin >> n;
-        for(int i = 0; i < 2; ++i)
+        for(int j = R; j <= L; ++j)
         {
-            for(int j = 1; j <= n; ++j) cin >> p[j][i];
+            for(int i = 1; i <= n; ++i)
+            {
+                cin >> p[i][j];
+            }
         }
-        cout << max({sol(n,0), sol(n,1), sol(n,2)}) << '\n';
+        for(int i = 1; i <= n; ++i) d[i].fill(-1);
+        cout << max({sol(n,N), sol(n,R), sol(n,L)}) << '\n';
     }
     return 0;
 }
