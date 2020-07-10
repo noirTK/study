16a54b00065123be5d73f0f945422351990f06d9
noir8TK <plaire@hanyang.ac.kr>
2020-07-10 13:04:03 +0900

    update 1309 TD && BU

--- cpp/알고리즘/1309_bu.cc
+++ cpp/알고리즘/1309_bu.cc
@@ -1,20 +1,31 @@
+/*
+https://www.acmicpc.net/problem/1309
+1309 다이나믹 문제풀이 Bottom Up
+d[n][s] = 2xn 배열의 n번째 자리에 사자가 위치하거나 위치하지 않았을 경우의 자리 배치 경우의 수
+d[n][N] = d[n-1][R] + d[n-1][L] + d[n-1][N]
+d[n][R] = d[n-1][L] + d[n-1][N]
+d[n][L] = d[n-1][R] + d[n-1][N]
+d[1][R] = d[1][L] = d[1][N] = 1
+*/
 #include <iostream>
 #include <array>
 
 using namespace std;
+enum space {N = 0, R, L};
 const int mod = 9901;
 array<array<int, 3>, 100001> d;
+
 int main()
 {
     int n;
     cin >> n;
-    d[1][0] = d[1][1] = d[1][2] = 1;
+    d[1][N] = d[1][R] = d[1][L] = 1;
     for(int i = 2; i <= n; ++i)
     {
-        d[i][0] = (d[i-1][1] + d[i-1][2]) % mod;
-        d[i][1] = (d[i-1][0] + d[i-1][2]) % mod;
-        d[i][2] = (d[i-1][0] + d[i-1][1] + d[i-1][2]) % mod;
+        d[i][N] = (d[i-1][R] + d[i-1][L] + d[i-1][N]) % mod;
+        d[i][L] = (d[i-1][R] + d[i-1][N]) % mod;
+        d[i][R] = (d[i-1][L] + d[i-1][N]) % mod;
     }
-    cout << (d[n][0] + d[n][1] + d[n][2]) % mod << '\n';
+    cout << (d[n][N] + d[n][R] + d[n][L]) % mod << '\n';
     return 0;
 }
--- cpp/알고리즘/1309_td.cc
+++ cpp/알고리즘/1309_td.cc
@@ -1,25 +1,37 @@
+/*
+https://www.acmicpc.net/problem/1309
+1309 다이나믹 문제풀이 Top Down
+sol(n,s) 2xn 배열의 n번째 자리에 사자가 위치하거나 위치하지 않았을 경우의 자리배치 경우의 수
+sol(n,N) = sol(n-1,R) + sol(n-1,L) + sol(n-1,N)
+sol(n,L) = sol(n-1,R) + sol(n-1,N)
+sol(n,R) = sol(n-1,L) + sol(n-1,N)
+sol(1,N) = sol(1,L) = sol(1,R) = 1
+*/
+
 #include <iostream>
 #include <array>
 
 using namespace std;
-array<array<int, 3>, 100001> d;
 const int mod = 9901;
-int sol(const int n, const int t)
+array<array<int, 3>, 100001> d;
+enum space { N = 0, R, L};
+
+int sol(const int n, const int s)
 {
     if(n == 1) return 1;
-    if(d[n][t] != -1) return d[n][t];
-    if(t == 0) d[n][t] = sol(n-1, 1) + sol(n-1, 2);
-    if(t == 1) d[n][t] = sol(n-1, 0) + sol(n-1, 2);
-    if(t == 2) d[n][t] = sol(n-1, 0) + sol(n-1, 1) + sol(n-1, 2);
-    d[n][t] %= mod;
-    return d[n][t];
+    if(d[n][s] != -1) return d[n][s];
+    if(s == N) d[n][s] = sol(n-1,R) + sol(n-1,L) + sol(n-1,N);
+    else if(s == L) d[n][s] = sol(n-1,R) + sol(n-1,N);
+    else if(s == R) d[n][s] = sol(n-1,L) + sol(n-1,N);
+    d[n][s] %= mod;
+    return d[n][s];
 }
 
 int main()
 {
-    for(int i = 1; i <= 100001; ++i) d[i].fill(-1);
     int n;
     cin >> n;
-    cout << (sol(n,0) + sol(n,1) + sol(n,2)) % mod << '\n'; 
-    return 0;
+    for(int i = 1; i <= n; ++i) d[i].fill(-1);
+    cout << (sol(n,N) + sol(n,R) + sol(n,L)) % mod << '\n';
+    return 0; 
 }
