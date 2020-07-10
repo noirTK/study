3a9e08b1a9b50ee6e665962730ace879ae90dd05
noir8TK <plaire@hanyang.ac.kr>
2020-07-10 21:16:07 +0900

    update 11057 TD && BU

--- cpp/알고리즘/11057_bu.cc
+++ cpp/알고리즘/11057_bu.cc
@@ -1,27 +1,42 @@
+/*
+https://www.acmicpc.net/problem/11057
+11057 다이나믹 문제풀이 Bottom Up
+d[n][t] 길이가 n이고 마지막 수가 t일 대의 오르막 수의 개수
+d[n][L] = Σ d[n-1][S] (0 <= S <= L)
+d[1][L] = 1 (0 <= L <= 9)
+ans = Σ d[n][L] (0 <= L <= 9)
+*/
+
 #include <iostream>
 #include <array>
 
-const int mod = 10007;
 using namespace std;
+const int mod = 10007;
 array<array<int, 10>, 1001> d;
+
 int main()
 {
     int n;
     cin >> n;
-    for(int i = 0; i < 10; ++i) d[1][i] = 1;
-    for(int i = 2; i <= n; ++i)
+    for(int i = 1; i <= n; ++i)
     {
-        for(int k = 0; k < 10; ++k)
+        for(int t = 0; t <= 9; ++t)
         {
-            for(int j = 0; j <= k; ++j)
+            if(i == 1) 
             {
-                d[i][k] += d[i-1][k-j];
-                d[i][k] %= mod;
+                d[i][t] = 1;
+                continue;
+            }
+            d[i][t] = 0;
+            for(int j = 0; j <= t; ++j)
+            {
+                d[i][t] += d[i-1][j];
+                d[i][t] %= mod;
             }
         }
     }
     int ans = 0;
-    for(int i = 0; i < 10; ++i)
+    for(int i = 0; i <= 9; ++i)
     {
         ans += d[n][i];
         ans %= mod;
--- cpp/알고리즘/11057_td.cc
+++ cpp/알고리즘/11057_td.cc
@@ -1,3 +1,11 @@
+/*
+https://www.acmicpc.net/problem/11057
+11057 다이나믹 문제풀이 TopDown
+sol(n,t) 길이가 n이고 마지막 수가 t일 때의 오르막 수의 개수
+sol(n,L) = Σ sol(n-1,S) (0 <= S <= L)
+sol(1,L) = 1 (0 <= L <= 9)
+ans = Σ sol(n,L) (0 <= L <= 9)
+*/
 #include <iostream>
 #include <array>
 
@@ -10,20 +18,21 @@ int sol(const int n, const int t)
     if(n == 1) return 1;
     if(d[n][t] != -1) return d[n][t];
     d[n][t] = 0;
-    for(int j = 0; j <= t; ++j)
+    for(int i = 0; i <= t; ++i)
     {
-        d[n][t] += sol(n-1,t-j);
+        d[n][t] += sol(n-1,i);
         d[n][t] %= mod;
     }
     return d[n][t];
 }
+
 int main()
 {
-    for(int i = 1; i < 1001; ++i) d[i].fill(-1);
     int n;
     cin >> n;
+    for(int i = 1; i <= n; ++i) d[i].fill(-1);
     int ans = 0;
-    for(int i = 0; i < 10; ++i)
+    for(int i = 0; i <= 9; ++i)
     {
         ans += sol(n,i);
         ans %= mod;
