b81d38274534f7048064f1e59360c352a074c75b
noir8TK <plaire@hanyang.ac.kr>
2020-07-11 00:51:21 +0900

    update 2156 TD, 2156_2 TD

--- cpp/알고리즘/2156_2_td.cc
+++ cpp/알고리즘/2156_2_td.cc
@@ -1,9 +1,31 @@
+/*
+https://www.acmicpc.net/problem/2156
+2156 다이나믹 문제풀이2 Top Down
+sol(n) n개의 포도주에서 규칙에 따라 포도주를 마실 경우, 마실 수 있는 포도주의 양의 최댓값
+sol(n) = max(sol(n-1), sol(n-2)+p[n], sol(n-3)+p[n-1]+p[n])
+sol(0) = 0
+sol(1) = p[1]
+sol(2) = p[1] + p[2]
+
+============================================================================
+sol(n) = max(sol(n-1), sol(n-2)+p[n], sol(n-3)+p[n-1]+p[n])
+1. sol(n-1) : n번째에서 포도주를 마시지 않는 경우이므로, n-1번째 포도주 양의 최댓값을 가져온다.
+2. sol(n-2)+p[n] : n번째에서 포도주를 1번 연속으로 마시는 경우이다.
+이때, 위 조건을 만족시키기 위해서는 n-1번째에서 포도주를 마시지 않아야 하므로,
+n-2번째 포도주 양의 최댓값에 n번째 포도주 양을 더한다.
+3. sol(n-3)+p[n-1]+p[n] : n번째에서 포도주를 2번 연속으로 마시는 경우이다.
+위 조건을 만족시키기 위해서는 n-2번째에서 포도주를 마시지 않아야 하므로,
+n-3번째 포도주 양의 최댓ㄱ밧에 n-1번째 포도주 양과 n번째 포도주 양을 더한다.
+============================================================================
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
 
 using namespace std;
 array<int, 10001> d, p;
+
 int sol(const int n)
 {
     if(n == 0) return 0;
@@ -16,9 +38,9 @@ int sol(const int n)
 
 int main()
 {
-    for(int i = 1; i <= 10001; ++i) d.fill(-1);
     int n;
     cin >> n;
+    d.fill(-1);
     for(int i = 1; i <= n; ++i) cin >> p[i];
     cout << sol(n) << '\n';
     return 0;
--- cpp/알고리즘/2156_td.cc
+++ cpp/알고리즘/2156_td.cc
@@ -1,26 +1,37 @@
+/*
+https://www.acmicpc.net/problem/2156
+2156 다이나믹 문제풀이 Top Down
+sol(n,c) n개의 포도주잔 중에서 마지막으로 c번째 마실 경우의 포도주 양의 최댓값
+sol(n,0) = max(sol(n-1,0), sol(n-1,1), sol(n-1,2));
+sol(n,1) = sol(n-1,0) + p[n]
+sol(n,2) = sol(n-1,1) + p[n]
+sol(1,1) = p[1]
+sol(1,0) = sol(1,2) = 0 
+*/
 #include <iostream>
 #include <array>
 #include <algorithm>
+
 using namespace std;
 array<array<int, 3>, 10001> d;
 array<int, 10001> p;
 
-int sol(const int n, const int t)
+int sol(const int n, const int c)
 {
-    if(n == 1) return (t == 1 ? p[1] : 0);
-    if(d[n][t] != -1) return d[n][t];
-    if(t == 0) d[n][t] = max({sol(n-1, 0), sol(n-1, 1), sol(n-1, 2)});
-    if(t == 1) d[n][t] = sol(n-1, 0) + p[n];
-    if(t == 2) d[n][t] = sol(n-1, 1) + p[n];
-    return d[n][t];
+    if(n == 1) return (c == 1) ? p[1] : 0;
+    if(d[n][c] != -1) return d[n][c];
+    if(c == 0) d[n][c] = max({sol(n-1,0), sol(n-1,1), sol(n-1,2)});
+    else if(c == 1) d[n][c] = sol(n-1,0) + p[n];
+    else if(c == 2) d[n][c] = sol(n-1,1) + p[n];
+    return d[n][c];
 }
 
 int main()
 {
-    for(int i = 1; i < 10001; ++i) d[i].fill(-1);
     int n;
     cin >> n;
     for(int i = 1; i <= n; ++i) cin >> p[i];
-    cout << max({sol(n,2), sol(n,1), sol(n,0)}) << '\n';
+    for(int i = 1; i <= n; ++i) d[i].fill(-1);
+    cout << max({sol(n,0), sol(n,1), sol(n,2)}) << '\n';
     return 0;
 }
