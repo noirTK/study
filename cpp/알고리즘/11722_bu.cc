b215740e22fa0fd2a3dd5744c0fa99b680a07132
noir8TK <plaire@hanyang.ac.kr>
2020-07-23 01:51:00 +0900

    update 11722 TD && BU

--- cpp/알고리즘/11722_bu.cc
+++ cpp/알고리즘/11722_bu.cc
@@ -1,9 +1,18 @@
+/*
+https://www.acmicpc.net/problem/11722
+11722 다이나믹 문제풀이 Bottom Up
+d[n] n번째 숫자를 선택했을 경우의 감소하는 부분수열의 길이
+d[n] = 1
+(p[n] < p[L]) d[n] = max(d[L]+1, d[n]) (1 <= L && L < n)
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
 
 using namespace std;
-array<int, 1001> p, d;
+array<int, 1001> d, p;
+
 int main()
 {
     int n;
@@ -12,11 +21,11 @@ int main()
     for(int i = 1; i <= n; ++i)
     {
         d[i] = 1;
-        for(int j = 1; j < i; ++j)
+        for(int l = 1; l < i; ++l)
         {
-            if(d[i] <= d[j] && p[j] > p[i]) d[i] = d[j] + 1;
+            if(p[i] < p[l]) d[i] = max(d[l]+1, d[i]);
         }
     }
-    cout << *max_element(d.cbegin(), d.cend()) << '\n';
+    cout << *max_element(d.cbegin()+1, d.cbegin()+1+n) << '\n'; 
     return 0;
 }
--- cpp/알고리즘/11722_td.cc
+++ cpp/알고리즘/11722_td.cc
@@ -1,25 +1,38 @@
+/*
+https://www.acmicpc.net/problem/11722
+11722 다이나믹 문제풀이 Top Down
+sol(n) n번째 숫자를 선택했을 경우의 감소하는 부분수열의 길이
+d[n] = 1
+(p[n] < p[L]) d[n] = max(sol(L)+1, d[n]) (1 <= L && L < n)
+for(int i = 1; i <= n; ++i) sol(i) 이용한 직접호출
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
+
 using namespace std;
 array<int, 1001> d, p;
+
 int sol(const int n)
 {
+    if(n == 1) d[n] = 1;
     if(d[n] != -1) return d[n];
     d[n] = 1;
-    for(int j = 1; j < n; ++j) 
+    for(int i = 1; i < n; ++i)
     {
-        if(p[n] < p[j] && d[n] <= sol(j)) d[n] = sol(j) + 1;
+        if(p[n] < p[i]) d[n] = max(sol(i)+1, d[n]);
     }
     return d[n];
 }
+
 int main()
 {
-    d.fill(-1);
     int n;
     cin >> n;
+    d.fill(-1);
     for(int i = 1; i <= n; ++i) cin >> p[i];
     for(int i = 1; i <= n; ++i) sol(i);
-    cout << *max_element(d.cbegin(), d.cend()) << '\n';
+    cout << *max_element(d.cbegin()+1, d.cbegin()+1+n) << '\n';
     return 0;
 }
