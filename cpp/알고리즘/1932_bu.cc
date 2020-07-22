f3c859ada032e00dc8b5319f6e5cb4a393e04f55
noir8TK <plaire@hanyang.ac.kr>
2020-07-22 23:20:18 +0900

    update 1932 TD && BU

--- cpp/알고리즘/1932_bu.cc
+++ cpp/알고리즘/1932_bu.cc
@@ -1,3 +1,13 @@
+/*
+https://www.acmicpc.net/problem/1932
+1932 다이나믹 문제풀이 Bottom Up
+d[n][l] 삼각형에서 (n,l)의 숫자를 선택했을 경우의 최댓값
+1. (l == 1) d[n][l] = d[n-1][l] + p[n][l]
+2. (l == n) d[n][l] = d[n-1][l-1] + p[n][l]
+3. (l > 1 && l < n) d[n][l] = max(d[n-1][l], d[n-1][l-1]) + p[n][l]
+d[1][1] = p[1][1]
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
@@ -9,22 +19,23 @@ int main()
 {
     int n;
     cin >> n;
-    for(int i = 1; i <= n; ++i)
+    for(int y = 1; y <= n; ++y)
     {
-        for(int j = 1; j <= i; ++j) cin >> p[i][j];
+        for(int x = 1; x <= y; ++x)
+        {
+            cin >> p[y][x];
+        }
     }
     d[1][1] = p[1][1];
     for(int i = 2; i <= n; ++i)
     {
-        for(int k = 1; k <= n; ++k)
+        for(int l = 1; l <= i; ++l)
         {
-            if(k == 1) d[i][k] = d[i-1][1] + p[i][1];
-            if(k == i) d[i][k] = d[i-1][i-1] + p[i][i];
-            else d[i][k] = max({d[i-1][k-1], d[i-1][k]}) + p[i][k];
+            if(l == 1) d[i][l] = d[i-1][l];
+            else if(l == i) d[i][l] = d[i-1][l-1];
+            else d[i][l] = max(d[i-1][l], d[i-1][l-1]);
+            d[i][l] += p[i][l];
         }
     }
-    int ans;
-    for(int i = 1; i <= n; ++i) if(i == 1 || ans < d[n][i]) ans = d[n][i];
-    cout << ans << '\n';
-    return 0;
+    cout << *max_element(&d[n][1], &d[n][1]+n) << '\n';
 }
--- cpp/알고리즘/1932_td.cc
+++ cpp/알고리즘/1932_td.cc
@@ -1,30 +1,47 @@
+/*
+https://www.acmicpc.net/problem/1932
+1932 다이나믹 문제풀이 Top Down
+sol(n,l) 삼각형에서 (n,l)의 숫자를 선택했을 경우의 최댓값
+1. (l == 1) sol(n,l) = sol(n-1,l) + p[n][l]
+2. (l == n) sol(n,l) = sol(n-1,l-1) + p[n][l]
+3. (l > 1 && l < n) sol(n,l) = max(sol(n-1,l), sol(n-1,l-1)) + p[n][l]
+sol(1,1) = p[1][1]
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
+
 using namespace std;
 array<array<int, 501>, 501> d, p;
 
-int sol(const int n, const int t)
+int sol(const int n, const int l)
 {
-    if(n == 1) return p[1][1];
-    if(d[n][t] != -1) return d[n][t];
-    if(t == 1) d[n][t] = sol(n-1, 1) + p[n][1];
-    if(t == n) d[n][t] = sol(n-1, n-1) + p[n][n];
-    else d[n][t] = max({sol(n-1, t-1), sol(n-1, t)}) + p[n][t];
-    return d[n][t];
+    if(n == 1) 
+    {
+        d[n][l] = p[n][l];
+        return p[n][l];
+    }
+    if(d[n][l] != -1) return d[n][l];
+    if(l == 1) d[n][l] = sol(n-1, l);
+    else if(l == n) d[n][l] = sol(n-1,l-1);
+    else d[n][l] = max(sol(n-1,l), sol(n-1,l-1));
+    d[n][l] += p[n][l];
+    return d[n][l];
 }
 
 int main()
 {
-    for(int i = 1; i < 501; ++i) d[i].fill(-1);
     int n;
     cin >> n;
-    for(int i = 1; i <= n; ++i)
+    for(int i = 1; i <= n; ++i) d[i].fill(-1);
+    for(int y = 1; y <= n; ++y)
     {
-        for(int j = 1; j <= i; ++j) cin >> p[i][j];
+        for(int x = 1; x <= y; ++x)
+        {
+            cin >> p[y][x];
+        }
     }
-    int ans;
-    for(int i = 1; i <= n; ++i) if(i == 1 || ans < sol(n, i)) ans = sol(n, i);
-    cout << ans << '\n';
-    return 0;
+    for(int i = 1; i <= n; ++i) sol(n,i);
+    cout << *max_element(&d[n][1], &d[n][1]+n) << '\n';
 }
