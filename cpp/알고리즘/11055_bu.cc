d39f65e6cfced9962a098f26e5101ef1a60b2005
noir8TK <plaire@hanyang.ac.kr>
2020-07-23 00:23:53 +0900

    update 11055 TD && BU

--- cpp/알고리즘/11055_bu.cc
+++ cpp/알고리즘/11055_bu.cc
@@ -1,8 +1,18 @@
+/*
+https://www.acmicpc.net/problem/11055
+11055 다이나믹 문제풀이 Bottom Up
+d[n] p[n]을 선택했을 경우의 증가 부분 수열의 최댓값
+d[n] = p[n]
+(p[n] > p[L]) d[n] = max(d[L]+p[n], d[n]) (L >= 1 && L < n)
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
+
 using namespace std;
 array<int, 1001> d, p;
+
 int main()
 {
     int n;
@@ -13,9 +23,9 @@ int main()
         d[i] = p[i];
         for(int j = 1; j < i; ++j)
         {
-            if(p[j] < p[i] && d[j] + p[i] > d[i]) d[i] = d[j] + p[i];
+            if(p[i] > p[j]) d[i] = max(d[j]+p[i], d[i]);
         }
     }
-    cout << *max_element(d.cbegin(), d.cend()) << '\n';
+    cout << *max_element(d.cbegin()+1, d.cbegin()+n+1) << '\n';
     return 0;
 }
--- cpp/알고리즘/11055_td.cc
+++ cpp/알고리즘/11055_td.cc
@@ -1,27 +1,35 @@
+/*
+https://www.acmicpc.net/problem/11055
+11055 다이나믹 문제풀이 Top Down
+sol(n) p[n]을 선택했을 경우의 증가 부분 수열의 최댓값
+sol(n) = p[n]
+sol(n) (p[n] > p[L]) d[n] = max(sol(L)+p[n], d[n]) (L >= 1 && L < n)
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
+
 using namespace std;
-array<int, 1001> p, d;
+array<int, 1001> d, p;
 
 int sol(const int n)
 {
-    if(n == 1) return p[n];
     if(d[n] != -1) return d[n];
     d[n] = p[n];
-    for(int i = 1; i < n; ++i) if(p[n] > p[i]) d[n] = max({sol(i) + p[n], d[n]});
+    for(int i = 1; i < n; ++i)
+    {
+        if(p[n] > p[i]) d[n] = max(sol(i)+p[n], d[n]);
+    }
     return d[n];
 }
 
 int main()
 {
-    for(int i = 1; i < 1001; ++i) d.fill(-1);
     int n;
     cin >> n;
+    d.fill(-1);
     for(int i = 1; i <= n; ++i) cin >> p[i];
-    int ans;
-    for(int i = 1; i <= n; ++i) 
-    if(i == 1 || ans < sol(i)) ans = sol(i);
-    cout << ans << '\n';
-    return 0;
+    for(int i = 1; i <= n; ++i) sol(i);
+    cout << *max_element(d.cbegin()+1, d.cbegin()+n+1) << '\n';
 }
