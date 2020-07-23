824b45172a38bc985fb7ab8251a9b2b6beb8cfb4
noir8TK <plaire@hanyang.ac.kr>
2020-07-23 23:25:31 +0900

    update 13398 TD && BU

--- cpp/알고리즘/13398_bu.cc
+++ cpp/알고리즘/13398_bu.cc
@@ -1,28 +1,46 @@
+/*
+https://www.acmicpc.net/problem/13398
+13398 다이나믹 문제풀이 Bottom Up
+1. u[n] : n번째 수를 선택할 경우의 연속합의 최댓값
+   u[n] = max(u[n-1]+p[n], p[n])
+2. d[n] : last에서 1로 1씩 감소할 때 n번째 수를 선택할 경우의 연속합의 최댓값
+   d[n] = max(d[n+1]+p[n],p[n])
+ans = max(u[L], ans) (L >= 1 && L <= n)
+   특정 수를 제외하지 않는 경우를 포함해야 하므로, 제외하지 않는 경우의 최댓값을 정한다.
+ans = max(d[L+1], ans) (L == 1)
+ans = max(u[L-1]+d[L+1], ans) (L > 1 && L < n)
+   이후 L번째 수를 제외하는 경우의 최댓값을 정한다.
+   L이 n일 때, ans와 u[L-1]를 비교해야하는데, 
+   이는 이미 특정 수를 제외하지 않는 경우에서 계산했으므로 의도적으로 제외했다.
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
+
 using namespace std;
-array<int, 100001> p, front, back;
+array<int, 100001> p, d, u;
+
 int main()
 {
-    int n;
+    int n, ans;
     cin >> n;
     for(int i = 1; i <= n; ++i) cin >> p[i];
-    int ans;
-    front[1] = p[1];
-    for(int i = 2; i <= n; ++i)
+    u[1] = p[1];
+    for(int i = 2; i <= n; ++i) 
     {
-        front[i] = (front[i-1] + p[i] > p[i] ? front[i-1] + p[i] : p[i]);
+        u[i] = max(u[i-1]+p[i], p[i]);
     }
-    for(int i = 1; i <= n; ++i) if(i == 1 || ans < front[i]) ans = front[i];
-    back[n] = p[n];
+    d[n] = p[n];
     for(int i = n-1; i >= 1; --i)
     {
-        back[i] = (back[i+1] + p[i] > p[i] ? back[i+1] + p[i] : p[i]);
+        d[i] = max(d[i+1]+p[i], p[i]);
     }
-    for(int i = 2; i < n; ++i)
+    ans = *max_element(u.cbegin()+1, u.cbegin()+n+1);
+    for(int i = 1; i < n; ++i)
     {
-        if(ans < front[i-1] + back[i+1]) ans = front[i-1] + back[i+1];
+        if(i == 1 && d[i+1] > ans) ans = d[i];
+        if(u[i-1] + d[i+1] > ans) ans = u[i-1] + d[i+1];
     }
     cout << ans << '\n';
     return 0;
--- cpp/알고리즘/13398_td.cc
+++ cpp/알고리즘/13398_td.cc
@@ -1,48 +1,56 @@
+/*
+https://www.acmicpc.net/problem/13398
+13398 다이나믹 문제풀이 Top Down
+1. solu(n) : n번째 수를 선택할 경우의 연속합의 최댓값
+   solu(n) = max(solu(n-1)+p[n], p[n])
+2. sold(n, last) : last에서 1로 1씩 감소할 때 n번째 수를 선택할 경우의 연속합의 최댓값
+   sold(n,last) = max(sold(n+1,last)+p[n], p[n])
+ans = max(solu(L), ans) (L >= 1 && L <= n)
+   특정 수를 제외하지 않는 경우를 포함해야 하므로, 제외하지 않는 경우의 최댓값을 정한다.
+ans = max(sold(L+1,n), ans) (L == 1)
+ans = max(solu(L-1)+sold(L+1,n), ans) (L > 1 && L < n)
+   이후 L번째 수를 제외하는 경우의 최댓값을 정한다.
+   L이 n일 때, ans와 solu(L-1)를 비교해야하는데, 
+   이는 이미 특정 수를 제외하지 않는 경우에서 계산했으므로 의도적으로 제외했다.
+*/
+
 #include <iostream>
 #include <array>
+#include <algorithm>
 
 using namespace std;
-array<int, 100001> p, front, back;
+array<int, 100001> d, u, p;
 
-int sol(const int n)
+int solu(const int n)
 {
-    if(n == 1) 
-    {
-        front[1] = p[1];
-        return p[1];
-    }
-    if(front[n] != -1) return front[n];
-    front[n] = (sol(n-1) + p[n] > p[n] ? sol(n-1) + p[n] : p[n]);
-    return front[n];
+    if(n == 1) u[n] = p[n];
+    if(u[n] != -1001) return u[n];
+    u[n] = max(solu(n-1)+p[n], p[n]);
+    return u[n];
 }
 
-int sol2(const int n, const int last)
+int sold(const int n, const int last)
 {
-    if(n == last) 
-    {
-        back[n] = p[n];
-        return p[n];
-    }
-    if(back[n] != -1) return back[n];
-    back[n] = (sol2(n+1, last) + p[n] > p[n] ? sol2(n+1, last) + p[n] : p[n]);
-    return back[n];
+    if(n == last) d[n] = p[n];
+    if(d[n] != -1001) return d[n];
+    d[n] = max(sold(n+1, last)+p[n], p[n]);
+    return d[n];
 }
+
 int main()
 {
-    front.fill(-1);
-    back.fill(-1);
-    int n;
+    int n, ans;
     cin >> n;
+    d.fill(-1001);
+    u.fill(-1001);
     for(int i = 1; i <= n; ++i) cin >> p[i];
-    int ans;
-    for(int i = 1; i <= n; ++i)
+    solu(n);
+    sold(1,n);
+    ans = *max_element(u.cbegin()+1, u.cbegin()+n+1);
+    for(int i = 1; i < n; ++i)
     {
-        if(i == 1 || ans < sol(i)) ans = sol(i);
-    }
-    for(int i = n; i >= 1; --i) sol2(i, n);
-    for(int i = 2; i < n; ++i)
-    {
-        if(ans < front[i-1] + back[i+1]) ans = front[i-1] + back[i+1];
+        if(i == 1 && solu(i) > ans) ans = solu(i);
+        if(solu(i-1)+sold(i+1,n) > ans) ans = solu(i-1)+sold(i+1,n);
     }
     cout << ans << '\n';
     return 0;
