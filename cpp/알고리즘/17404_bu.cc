4453668e849abb54caa7c86dfcbaacb563eae166
noir8TK <plaire@hanyang.ac.kr>
2020-07-24 21:19:26 +0900

    update 17404 TD && BU

--- cpp/알고리즘/17404_bu.cc
+++ cpp/알고리즘/17404_bu.cc
@@ -1,31 +1,51 @@
+/*
+https://www.acmicpc.net/problem/17404
+17404 다이나믹 문제풀이 Bottom Up
+d[n][color] : 마지막을 last로 색칠할 때, n번째에 color을 색칠할 경우의 색칠 비용의 최솟값
+1. d[1][color]에서 color를 하나 선택할 경우, 무조건 특정 색깔을 처음에 선택해야 하므로, 나머지는 조건범위(1000 이하)보다 큰 수 1001로 초기화한다.
+2. d[i][0] = min(d[i-1][1], d[i-1][2]) + p[i][0]
+   d[i][1] = min(d[i-1][0], d[i-1][2]) + p[i][1]
+   d[i][2] = min(d[i-1][0], d[i-1][1]) + p[i][2]
+   (단, i >= 2 && i <= n)
+3. 마지막은 처음에 선택할 색깔을 제외한 나머지에서 최솟값을 선택해야하므로, 
+   처음에 선택했던 색 = color와 다른 경우를 각각 비교해서 최솟값을 ans에 할당한다.
+   cf. (color == 0) ans = min(d[n][1], d[n][2])
+       (color == 1) ans = min(d[n][0], d[n][2])
+       (color == 2) ans = min(d[n][0], d[n][1])
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
 
 using namespace std;
 array<array<int, 3>, 1001> d, p;
+
 int main()
 {
-    int n;
+    int n, ans;
     cin >> n;
-    for(int i = 1; i <= n; ++i) cin >> p[i][0] >> p[i][1] >> p[i][2];
-    int ans;
-    for(int k = 0; k <= 2; ++k)
+    for(int i = 1; i <= n; ++i)
     {
-        for(int i = 0; i <= 2; ++i)
-        {
-            if(k == i) d[1][i] = p[1][k];
-            else d[1][i] = 1000*1000+1;
-        }
+        cin >> p[i][0] >> p[i][1] >> p[i][2];
+    }
+
+    for(int color = 0; color < 3; ++color)
+    {
+        d[1][0] = d[1][1] = d[1][2] = 1001;
+        d[1][color] = p[1][color];
+
         for(int i = 2; i <= n; ++i)
         {
             d[i][0] = min(d[i-1][1], d[i-1][2]) + p[i][0];
             d[i][1] = min(d[i-1][0], d[i-1][2]) + p[i][1];
             d[i][2] = min(d[i-1][0], d[i-1][1]) + p[i][2];
         }
-        if(k == 0) ans = min(d[n][1], d[n][2]);
-        if(k == 1 && ans > min(d[n][0], d[n][2])) ans = min(d[n][0], d[n][2]);
-        if(k == 2 && ans > min(d[n][0], d[n][1])) ans = min(d[n][0], d[n][1]);
+        for(int i = 0; i < 3; ++i)
+        {
+            if(color == i) continue;
+            ans = min(ans, d[n][i]);
+        }
     }
     cout << ans << '\n';
     return 0;
--- cpp/알고리즘/17404_td.cc
+++ cpp/알고리즘/17404_td.cc
@@ -1,31 +1,46 @@
+/*
+https://www.acmicpc.net/problem/17404
+17404 다이나믹 문제풀이 Top Down
+sol(n, color, last) : 마지막을 last로 색칠할 때, n번째에 color을 색칠할 경우의 색칠 비용의 최솟값
+sol(n, 0, last) = min(sol(n-1, 1, last), sol(n-1, 2, last)) + p[n][color]
+sol(n, 1, last) = min(sol(n-1, 0, last), sol(n-1, 2, last)) + p[n][color]
+sol(n, 2, last) = min(sol(n-1, 0, last), sol(n-1, 1, last)) + p[n][color]
+sol(1, color, last)에서, color와 last색이 같을 경우, 원형 RGB 조건에 어긋나므로, 이 경우, 조건값의 범위(1000 이하)보다 더 큰 수를 리턴하여 해당 수를 선택하지 않도록 한다.
+
+ans = last에 0색, 1색, 2색을 각각 선택할 경우의 최솟값 이므로,
+ans = min(sol(n,0,0), sol(n,1,1), sol(n,2,2))이다.
+이 때, 각각 경우에 따라 값이 달라지므로, -1로 초기화 이후에 값을 구한다.
+*/
+
 #include <iostream>
 #include <array>
 #include <algorithm>
+
 using namespace std;
 array<array<int, 3>, 1001> d, p;
-int sol(const int n, const int c, const int last)
+
+int sol(const int n, const int color, const int last)
 {
-    if(n == 1)
-    {
-        if(c == last) return 1001;
-        else return p[1][c];
-    }
-    if(d[n][c] != -1) return d[n][c];
-    if(c == 0) d[n][c] = min(sol(n-1,1,last), sol(n-1,2,last)) + p[n][c];
-    if(c == 1) d[n][c] = min(sol(n-1,0,last), sol(n-1,2,last)) + p[n][c];
-    if(c == 2) d[n][c] = min(sol(n-1,0,last), sol(n-1,1,last)) + p[n][c];
-    return d[n][c];
+    if(n == 1) return (color == last) ? 1001 : p[n][color];
+    if(d[n][color] != -1) return d[n][color];
+    if(color == 0) d[n][color] = min(sol(n-1,1,last),sol(n-1,2,last));
+    else if(color == 1) d[n][color] = min(sol(n-1,0,last),sol(n-1,2,last));
+    else if(color == 2) d[n][color] = min(sol(n-1,0,last), sol(n-1,1,last));
+    d[n][color] += p[n][color];
+    return d[n][color];
 }
 
 int main()
 {
-    int n;
+    int n, ans;
     cin >> n;
-    for(int i = 1; i <= n; ++i) cin >> p[i][0] >> p[i][1] >> p[i][2];
-    int ans;
+    for(int i = 1; i <= n; ++i)
+    {
+        cin >> p[i][0] >> p[i][1] >> p[i][2];
+    }
     for(int i = 0; i <= 2; ++i)
     {
-        for(int j = 1; j < 1001; ++j) d[j].fill(-1);
+        for(int i = 1; i <= n; ++i) d[i].fill(-1);
         if(i == 0 || ans > sol(n,i,i)) ans = sol(n,i,i);
     }
     cout << ans << '\n';
